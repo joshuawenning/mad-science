77e4fa7d5703be4cffeb26e29eb07385492e1270
Joshua Wenning <joshuawenning@gmail.com>
2025-12-15 10:24:11 -0500

    Fix name

--- basic_exercises.rb
+++ basic_exercises.rb
@@ -1,5 +1,5 @@
 # Add two strings together that, when concatenated, return your first and last name as your full name in one string.
-puts "Advait " + "Chaitanya"
+puts "Joshua " + "Wenning"
 
 # Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
 number = 4321
